955a397d4afa31c0487d793e28474d17d4f28a76
yanik23 <ylange@windowslive.com>
2021-04-30 11:14:52 +0200

    adding Group class

--- src/main/java/mail/Group.java
+++ src/main/java/mail/Group.java
@@ -1,4 +1,22 @@
 package mail;
 
+import java.util.List;
+
 public class Group {
+
+    private Person from;
+    private List<Person> to;
+
+    public Group(Person from, List<Person> to){
+        this.from = from;
+        this.to = to;
+    }
+
+    public Person getFrom(){
+        return from;
+    }
+
+    public List<Person> getTo(){
+        return to;
+    }
 }
